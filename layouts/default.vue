de0e794b0050a3bc3635254dc393ace71c3cb9f3
Jeff Spurlock <jeffspurlock@icloud.com>
2024-01-27 20:16:48 -0600

    still trying to fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import Leftnav from "~/components/leftnav.vue"
+import Blognav from "~/components/blognav.vue";
+
 const globalState = useGlobalState()
 const contentId = ref()
 const pagesWithOverflow = ['/about']
